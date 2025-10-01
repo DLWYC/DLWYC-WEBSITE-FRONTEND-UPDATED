cdcd09ad1aab1bddac4a641f4413a39dca36e8d8
DLWYC <dlwyouth@gmail.com>
2025-10-01 13:58:41 +0100

    Changing 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from '@tanstack/react-router'
-import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { useAuth } from '@/lib/AuthContext'
