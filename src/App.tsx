f70bc26a1df45271a05f2b264230071b28d127e5
DLWYC <dlwyouth@gmail.com>
2025-10-01 13:56:51 +0100

    Change Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { useAuth } from '@/lib/AuthContext'
