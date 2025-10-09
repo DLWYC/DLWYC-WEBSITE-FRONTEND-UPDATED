30ecaba29978339daa15d1126c55e002b9ceae41
Timmy-Tee <timmyaof02@gmail.com>
2025-10-09 20:09:35 +0100

    Updated Sections

--- src/routes/userlogin.jsx
+++ src/routes/userlogin.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import {useAuth} from "@/lib/AuthContext"
+import { Eye, EyeOff } from 'lucide-react';
 
 export const Route = createFileRoute('/userlogin')({
   component: RouteComponent,
@@ -12,8 +13,10 @@ export const Route = createFileRoute('/userlogin')({
 
 function RouteComponent() {
   const {login, loginIsLoading} = useAuth()
+  
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [password, setPassword] = useState("");
   const values = {
     email: email.toLowerCase(),
@@ -37,6 +40,7 @@ function RouteComponent() {
          navigate({to: '/userdashboard'});
       }
       catch(error){
+        console.log("This is error at Login", error)
         toast.error(error?.error || "Login Failed🤧")
       }
       
@@ -52,7 +56,7 @@ function RouteComponent() {
 
 return (
     <>
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 font-rubik">
       <div className="w-full max-w-md p-6 bg-white rounded-2xl shadow-lg">
         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
         <div className="space-y-4">
@@ -64,13 +68,24 @@ return (
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
 
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
+      
+          <div className="relative">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className={`text-[14px] w-full px-4  py-3 border  'border-gray-300' rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition`}
+              placeholder="Enter your password"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600"
+            >
+              {showPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
+            </button>
+          </div>
 
           <button
             // type="submit"
@@ -83,13 +98,13 @@ return (
         </div>
 
         {/* Google OAuth */}
-        <div className="mt-4 flex ">
+        {/* <div className="mt-4 flex ">
           <GoogleLogin
             onSuccess={handleGoogleLogin}
             // flow: 'auth-code',
             onError={() => alert("Google Login Failed")}
           />
-        </div>
+        </div> */}
 
         <p className="text-[15px] mt-4 text-center">
           Don’t have an account?{" "}
